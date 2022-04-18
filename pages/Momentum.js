817c2e704e4685e43f2c5bfc4cd2bfaeeb0c2915
Doohyun Cho <zodist92@gmail.com>
2022-04-19 00:31:56 +0900

    update momentum page style

--- pages/Momentum.js
+++ pages/Momentum.js
@@ -7,26 +7,28 @@ export default class {
     }
     async getHtml() {
         return `
-        <form id="login-form" class="hidden">
-            <input required type="text" placeholder="what is your name?" />
-            <button>Log In</button>
-        </form>
-        <h2 id="clock">00:00:00</h2>
-        <h1 id="greeting" class="hidden"></h1>
+        <div id="momentum">
+            <form id="login-form" class="hidden">
+                <input required type="text" placeholder="what is your name?" />
+                <button>Log In</button>
+            </form>
+            <h2 id="clock">00:00:00</h2>
+            <h1 id="greeting" class="hidden"></h1>
 
-        <form id="todo-form">
-            <input type="text" placeholder="Write a Todo and Press Enter" required />
-        </form>
-        <ul id="todo-list"></ul>
-        <div id="quote">
-            <span></span>
-            <br>
-            <span></span>
-        </div>
-        <div id="weather">
-            <span></span>
-            <br>
-            <span></span>
+            <form id="todo-form">
+                <input type="text" placeholder="Write a Todo and Press Enter" required />
+            </form>
+            <ul id="todo-list"></ul>
+            <div id="quote">
+                <span></span>
+                <br>
+                <span></span>
+            </div>
+            <div id="weather">
+                <span></span>
+                <br>
+                <span></span>
+            </div>
         </div>
         `;
     }
