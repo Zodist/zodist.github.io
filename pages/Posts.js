aa604802e4d399d6bd00eba9ba46d3c5fd569f40
Doohyun Cho <zodist92@gmail.com>
2022-04-30 15:44:19 +0900

    post page update

--- pages/Posts.js
+++ pages/Posts.js
@@ -4,7 +4,12 @@ export default class {
     }
     async getHtml() {
         return `
-            <h1>This is Posts Page</h1>
+            <h1>This is Posts Page (Test)</h1>
+            <div class="post">
+                <div class="postPage">Scroll</div>
+                <div class="postPage">Snap</div>
+                <div class="postPage">Test</div>
+            </div>
         `;
     }
 }
